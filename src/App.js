4788a51481753c374954f1b6d3f770f629dbfee1
Hải Yến <yenlthde160549@fpt.edu.vn>
2024-06-03 05:11:27 +0700

    FE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Categories from "./components/Categories";
+import Product from "./components/Product";
+
+import Discount from "./components/Discount";
+import Services from "./components/Services";
+import Facebook from "./components/Facebook";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Categories />
+      <Product />
+      <Discount />
+      <Services />
+      <Facebook />
+      <Footer />
     </div>
   );
 }
